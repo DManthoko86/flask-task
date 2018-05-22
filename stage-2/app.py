619793c536bb27d57236e59ea47fc16a6365352e
Dominic Manthoko <d.manthoko@gmail.com>
2018-05-22 15:26:00 +0200

    Completed task 2

--- stage-2/app.py
+++ stage-2/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
 from data import Transactions
 
-#export FLASK_APP=hello.py
+#export FLASK_APP=app.py
 #export FLASK_ENV=development
 #flask run
+#/mnt/c/Users/Dominic/Documents/flask-task/stage-2
 
 app = Flask(__name__)
 
