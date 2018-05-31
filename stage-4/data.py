d9202de5a317ae7f64fac3bc19f70357830c0785
Dominic Manthoko <d.manthoko@gmail.com>
2018-05-31 18:02:57 +0200

    Copied code from stage-3 to stage-4

--- stage-4/data.py
+++ stage-4/data.py
@@ -1,5 +1,33 @@
 import csv
 
+def Transactions():
+	#use to store the transaction data
+	transactions = [
+		{
+			'id':1,
+			'Date':'20-05-2018',
+			'Description':'Mcd',
+			'Amount':'40',
+			'Balance': 500
+		},
+		{
+			'id':2,
+			'Date':'21-05-2018',
+			'Description':'Ster-kinekor',
+			'Amount':'97',
+			'Balance': 403
+		},
+		{
+			'id':3,
+			'Date':'21-05-2018',
+			'Description':'Uber',
+			'Amount':'30',
+			'Balance': 373
+		}
+	]
+
+	return transactions
+
 def getTransactions():
 	file = open('data.csv', 'r+')
 	# balance = file.readline()
